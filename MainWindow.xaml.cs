4e7f5f4febb1388b660f26ef8ad490599bd12f10
Rafael Lins <rafiinhaliins@gmail.com>
2026-09-07 17:21:51 -0300

    feat: adciona sidebar retratil

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,7 +17,10 @@ namespace ADCoins
 
             if (sidebarAberta)
             {
+                // =========================
                 // ABRIR SIDEBAR
+                // =========================
+
                 SidebarColumn.Width = new GridLength(220);
 
                 LogoPanel.Visibility = Visibility.Visible;
@@ -29,11 +32,25 @@ namespace ADCoins
 
                 VersionText.Visibility = Visibility.Visible;
 
+                // Restaurar alinhamento
+                DashboardButton.HorizontalContentAlignment = HorizontalAlignment.Left;
+                ImbuementsButton.HorizontalContentAlignment = HorizontalAlignment.Left;
+                HuntButton.HorizontalContentAlignment = HorizontalAlignment.Left;
+                ConfigButton.HorizontalContentAlignment = HorizontalAlignment.Left;
+
+                DashboardButton.Padding = new Thickness(15, 0, 0, 0);
+                ImbuementsButton.Padding = new Thickness(15, 0, 0, 0);
+                HuntButton.Padding = new Thickness(15, 0, 0, 0);
+                ConfigButton.Padding = new Thickness(15, 0, 0, 0);
+
                 MenuButton.Content = "☰";
             }
             else
             {
+                // =========================
                 // FECHAR SIDEBAR
+                // =========================
+
                 SidebarColumn.Width = new GridLength(70);
 
                 LogoPanel.Visibility = Visibility.Collapsed;
@@ -44,6 +61,17 @@ namespace ADCoins
                 ConfigText.Visibility = Visibility.Collapsed;
 
                 VersionText.Visibility = Visibility.Collapsed;
+
+                // Centralizar os ícones
+                DashboardButton.HorizontalContentAlignment = HorizontalAlignment.Center;
+                ImbuementsButton.HorizontalContentAlignment = HorizontalAlignment.Center;
+                HuntButton.HorizontalContentAlignment = HorizontalAlignment.Center;
+                ConfigButton.HorizontalContentAlignment = HorizontalAlignment.Center;
+
+                DashboardButton.Padding = new Thickness(0);
+                ImbuementsButton.Padding = new Thickness(0);
+                HuntButton.Padding = new Thickness(0);
+                ConfigButton.Padding = new Thickness(0);
 
                 MenuButton.Content = "☰";
             }
